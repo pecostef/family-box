e009a277f8ee95a185f1e64a1e9c76256809af0b
Stefano Pecoraro <pecostef@gmail.com>
2024-10-04 18:50:40 +0200

    Add files via upload

---
+++ bundle.cjs.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class EmployeeSearchService {
+    constructor(searchEmployee) {
+        this.searchEmployee = searchEmployee;
+    }
+    async searchEmployees(search) {
+        if (!search || search.length <= 3) {
+            return [];
+        }
+        try {
+            const result = await this.searchEmployee.run({ search });
+            const options = result
+                .filter((u) => u.displayName && u.mail && !u.id.endsWith("_adm"))
+                .map((u) => ({ label: u.displayName, value: u.mail }));
+            return options;
+        }
+        catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+}
+const createEmployeeSearchService = (dep) => {
+    return new EmployeeSearchService(dep);
+};
+
+exports.createEmployeeSearchService = createEmployeeSearchService;
